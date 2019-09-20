9a2804374450dc89d4fdb33bb3f0288318a314a1
olanetsoft <heedris2olubisi@gmail.com>
2019-09-20 12:34:56 +0100

    files commited

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox';
+import { robots } from './robots';
+import './App.css';
+
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            robots: robots,
+            searchField: ''
+
+        }
+    } 
+    onSearchChange = (event) => {
+        this.setState({ searchField: event.target.value }) 
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        })
+        return(
+            <div className='tc'>
+                <h1 className='f1'>Robo Friends</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList robots={filteredRobots}/>  
+            </div>
+        
+    );
+    }
+}
+
+export default App; 
